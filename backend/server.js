24320b352da804d56c4841ca9422bab2c723a65f
frankciardo <162804939+Morrix76@users.noreply.github.com>
2025-10-28 17:48:17 +0100

    Fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,11 @@ const PORT = process.env.PORT || 3003;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:3001', 
+    'http://localhost:3000',
+    'https://taxpilot-en-git-main-franks-projects-c85cd5ad.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
