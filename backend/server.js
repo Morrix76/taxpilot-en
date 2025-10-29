0296449365e6757cfd1135b1fb74ff4a77ed52a4
frankciardo <162804939+Morrix76@users.noreply.github.com>
2025-10-29 14:28:05 +0100

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 8080;
 
 // ====== CORS ======
 app.use(cors({
